fb8d8b1f22365dc699ac8dd1a3316507420a7a2a
KongQingXin12 <john920817@outlook.com>
2020-01-01 18:08:56 +0800

    Python 解答

--- 动态规划/32. 最长有效括号.py
+++ 动态规划/32. 最长有效括号.py
@@ -1,9 +1,23 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         stack = []
-        res = 0
-        t = 0
-        for Str in s:
-            while stack and Str == ')':
-                t += 2
-                stack.pop()
+        res = [0]
+        for i, ss in enumerate(s):
+            if ss == ')':
+                if stack:
+                    if res[-1] == 0:
+                        res.append(2)
+                    else:
+                        res[-1] += 2
+                    stack.pop()
+                else:
+                    res.append(0)
+            else:
+                stack.append('(')
+        return max(res)
+
+
+if __name__ == '__main__':
+    s = ")()())"
+    S = Solution()
+    print(S.longestValidParentheses(s))
